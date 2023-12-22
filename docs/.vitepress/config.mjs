671e9d7852817096068fabb20ebbf3e348827b88
xingwei <xingwei@i-click.com>
2023-12-22 21:43:07 +0800

    初始化项目

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -17,7 +17,20 @@ export default defineConfig({
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: '数组展开', link: '/array-flatten' },
+          { text: 'promise', link: '/promise' },
+          { text: 'vite', link: '/vite' },
+          { text: 'webpack', link: '/webpack' },
+          { text: 'html&css', link: '/html&css'},
+          { text: 'javascript', link: '/javascript'},
+          { text: 'typescript', link: '/typescript'},
+          { text: 'vue', link: '/vue'},
+          { text: 'browser', link: '/browser'},
+          { text: 'other', link: '/other'},
+          { text: 'handwriting', link: '/handwriting'},
+          { text: 'vue2', link: '/vue2'},
+          { text: 'vue-router', link: '/vue-router'}
         ]
       }
     ],
